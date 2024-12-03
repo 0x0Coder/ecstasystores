52464dbac4d9e93144530049eb14d21f1e241996
anas Fred <anasfred3@gmail.com>
2024-12-03 07:28:51 +0100

    Update page.jsx

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -25,7 +25,7 @@ export default function page() {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/checkout', {
+      const response = await fetch('https://ecstasystoresbackendd.vercel.app/api/checkout', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
